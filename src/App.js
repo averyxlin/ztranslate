c3a0e7ae84e05a0f69b09feec3d6964ff5dee9a5
Personal <xinyan.lin@uwaterloo.ca>
2024-09-02 03:32:51 -0400

    configured isTranslate prop

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import TextArea from './components/TextArea';
 
 function App() {
   return (
-    <div className="h-dvh w-dvw flex items-center justify-center">
-      <TextArea/>
+    <div className="h-dvh w-dvw flex items-center justify-center gap-4">
+      <TextArea isTranslate/>
+      <TextArea />
     </div>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,33 +1,37 @@
 import React from 'react'
 
-const TextArea = () => {
+const TextArea = ({isTranslate}) => {
   return (
     <div>
         <label htmlFor="OrderNotes" className="sr-only">Order notes</label>
-        <div
-            className="overflow-hidden rounded-lg border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-        >
-            <textarea
-                id="TranslateInput"
-                className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm"
-                rows="4"
-                placeholder="Translate..."
-            ></textarea>
-
-            <div className="flex items-center justify-end gap-2 bg-white p-3">
-            <button
-                type="button"
-                className="rounded bg-gray-200 px-3 py-1.5 text-sm font-medium text-gray-700 hover:text-gray-600"
+        <div className="h-screen flex items-center justify-center">
+            <div
+                className="overflow-hidden rounded-lg border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
             >
-                Clear
-            </button>
+                <textarea
+                    id="TranslateInput"
+                    className="h-full w-full resize-none border-none align-top focus:ring-0 sm:text-sm p-3"
+                    rows="4"
+                    placeholder={isTranslate ? `Translate...` : `Translated Text`}
+                    disabled={!isTranslate}
+                ></textarea>
+                {isTranslate && 
+                    <div className="flex items-center justify-end gap-2 bg-white p-3">
+                        <button
+                            type="button"
+                            className="rounded bg-gray-200 px-3 py-1.5 text-sm font-medium text-gray-700 hover:text-gray-600"
+                        >
+                            Clear
+                        </button>
 
-            <button
-                type="button"
-                className="rounded bg-indigo-600 px-3 py-1.5 text-sm font-medium text-white hover:bg-indigo-700"
-            >
-                Translate
-            </button>
+                        <button
+                            type="button"
+                            className="rounded bg-indigo-600 px-3 py-1.5 text-sm font-medium text-white hover:bg-indigo-700"
+                        >
+                            Translate
+                        </button>
+                    </div>
+                }
             </div>
         </div>
     </div>
